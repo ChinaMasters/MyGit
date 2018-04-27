c87f747fb098642c2a6fc381cee7cdcf2e065814
taotao <1263641416@qq.com>
2018-04-27 13:37:38 +0800

    very goog!

--- test.js
+++ test.js
@@ -2,3 +2,4 @@ Git is a version control system.
 Git is free software distributed under the GPL;
 Git ha a mutable index called stage;
 Git is a distributed version control system;
+Gie task  changes;
